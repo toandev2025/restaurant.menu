540255982920d5928756929f6d6840e4abe6165f
Xjanua <ktoanhutech@gmail.com>
2025-03-03 21:37:55 +0700

    [firstPush]

---
+++ src/main/java/com/restaurant/menu/menu_management/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.restaurant.menu.menu_management.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getHelloWorld() {
+        return "Hello World (Hỏi Dân IT & Eric)";
+    }
+}
